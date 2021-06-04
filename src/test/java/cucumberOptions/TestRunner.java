870d8d38b685520f1db5ff87cd29d722902a92ea
Tufail Ashraf Kalsekar <tufaelk@gmail.com>
2021-06-04 17:02:33 +0530

    Update TestRunner.java

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src\\test\\java\\features", // Pass the path to features file
-		glue = "stepDefinations") // pass name of package that holds the definition file. note: it should be part of same folder.
+		glue = "stepDefinations") // pass name of package that holds the definition file. NOTE: it should be part of same folder.
 
 public class TestRunner {
 
